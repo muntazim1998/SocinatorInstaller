0f6775198108e6f41bfc636decea75d58a38023a
muntazim1998 <muntazimalikhan@globussoft.in>
2024-08-16 18:22:34 +0530

    ++Updated Codes++

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,22 +18,24 @@ namespace SocinatorInstaller
     /// </summary>
     public partial class MainWindow : Window, INotifyPropertyChanged
     {
-        private bool isLaunch=true;
+        private bool isLaunch = true;
         string[] installedInfo = new string[3];
-        string appName="Social Dominator";
-
+        string appName = "Social Dominator";
+        private int NxtButtonCount = 1;
+        private int BackButtonCount = 1;
         public event PropertyChangedEventHandler? PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
-        private double _step1Opacity=1.0;
+        private double _step1Opacity = 1.0;
 
         public double Step1Opacity
         {
             get { return _step1Opacity; }
-            set {
+            set
+            {
                 if (_step1Opacity != value)
                 {
                     _step1Opacity = value;
@@ -41,46 +43,49 @@ namespace SocinatorInstaller
                 }
             }
         }
-        private double _step2Opacity =0.5;
+        private double _step2Opacity = 0.5;
 
         public double Step2Opacity
         {
             get { return _step2Opacity; }
-            set {
+            set
+            {
                 if (_step2Opacity != value)
                 {
                     _step2Opacity = value;
                     OnPropertyChanged();
                 }
-                }
             }
-        private double _step3Opacity=0.5;
+        }
+        private double _step3Opacity = 0.5;
 
         public double Step3Opacity
         {
             get { return _step3Opacity; }
-            set {
+            set
+            {
                 if (_step3Opacity != value)
                 {
 
                     _step3Opacity = value;
                     OnPropertyChanged();
                 }
-                }
             }
-        private double _step4Opacity=0.5;
+        }
+        private double _step4Opacity = 0.5;
 
         public double Step4Opacity
         {
             get { return _step4Opacity; }
-            set {
+            set
+            {
                 if (_step4Opacity != value)
                 {
                     _step4Opacity = value;
                     OnPropertyChanged();
                 }
-                }
             }
+        }
         private int step1Width = 40;
 
         public int Step1Width
@@ -95,7 +100,7 @@ namespace SocinatorInstaller
             get { return step2Width; }
             set { step2Width = value; OnPropertyChanged(); }
         }
-        
+
         private int step3Width = 20;
 
         public int Step3Width
@@ -103,7 +108,7 @@ namespace SocinatorInstaller
             get { return step3Width; }
             set { step3Width = value; OnPropertyChanged(); }
         }
-        
+
         private int step4Width = 20;
 
         public int Step4Width
@@ -111,8 +116,8 @@ namespace SocinatorInstaller
             get { return step4Width; }
             set { step4Width = value; OnPropertyChanged(); }
         }
-        Color color1= (Color)ColorConverter.ConvertFromString("#B0CB0E");
-        Color color2= (Color)ColorConverter.ConvertFromString("#38BBC8");
+        Color color1 = (Color)ColorConverter.ConvertFromString("#B0CB0E");
+        Color color2 = (Color)ColorConverter.ConvertFromString("#38BBC8");
 
         LinearGradientBrush gradientBrush = new LinearGradientBrush
         {
@@ -137,13 +142,71 @@ namespace SocinatorInstaller
 
         private void click_NextBtn(object sender, RoutedEventArgs e)
         {
-            Step1Width = 20;
-            Step1Opacity = 0.5;
-            Step2Width = 40;
-            Step2Opacity = 1;
-            BackBtnBorder.Visibility = Visibility.Visible;
-            StartGrid.Visibility = Visibility.Collapsed;
-            DownloaderGrid.Visibility = Visibility.Visible;
+            if (NxtButtonCount == 1)
+            {
+                StartGrid.Visibility = Visibility.Collapsed;
+                DownloadOptionGrid.Visibility = Visibility.Visible;
+                BackBtnBorder.Visibility = Visibility.Visible;
+                Step1Width = 20;
+                Step1Opacity = 0.5;
+                Step2Width = 40;
+                Step2Opacity = 1;
+                NxtButtonCount++;
+                BackButtonCount = 1;
+            }
+            else if (NxtButtonCount == 2)
+            {
+                DownloadOptionGrid.Visibility = Visibility.Collapsed;
+                ConfirmationGrid.Visibility = Visibility.Visible;
+                Step2Width = 20;
+                Step2Opacity = 0.5;
+                Step3Width = 40;
+                Step3Opacity = 1;
+                BackButtonCount = 2;
+                NxtButtonCount++;
+            }
+            else if (NxtButtonCount == 3)
+            {
+                ConfirmationGrid.Visibility = Visibility.Collapsed;
+                //LOGIC FOR INSTALLATION COMPLETE
+                InstallationCompleteGrid.Visibility = Visibility.Visible;
+                Step3Width = 20;
+                Step3Opacity = 0.5;
+                Step4Width = 40;
+                Step4Opacity = 1;
+                CancelBtnBorder.Visibility = Visibility.Collapsed;
+                BackBtnBorder.Visibility = Visibility.Collapsed;
+                NextBtnBorder.Visibility = Visibility.Collapsed;
+                CloseBtnBorder.Visibility = Visibility.Visible;
+                NxtButtonCount++;
+            }
+
+        }
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (BackButtonCount == 2)
+            {
+                ConfirmationGrid.Visibility = Visibility.Collapsed;
+                DownloadOptionGrid.Visibility = Visibility.Visible;
+                Step3Width = 20;
+                Step3Opacity = 0.5;
+                Step2Width = 40;
+                Step2Opacity = 1;
+                NxtButtonCount = 2;
+                BackButtonCount--;
+            }
+            else  if (BackButtonCount == 1)
+            {
+                DownloadOptionGrid.Visibility = Visibility.Collapsed;
+                StartGrid.Visibility = Visibility.Visible;
+                BackBtnBorder.Visibility = Visibility.Collapsed;
+                Step1Width = 40;
+                Step1Opacity = 1;
+                Step2Width = 20;
+                Step2Opacity = 0.5;
+                NxtButtonCount=1;
+                BackButtonCount=0;
+            }
         }
 
         private void click_CloseBtn(object sender, RoutedEventArgs e)
@@ -156,7 +219,7 @@ namespace SocinatorInstaller
 
         }
 
-        
+
 
         private void click_BackBtn(object sender, RoutedEventArgs e)
         {
@@ -197,9 +260,9 @@ namespace SocinatorInstaller
         {
 
         }
-        private void ShowMessageBoxModel(bool isyes=false,string msg="",bool isAsync=false)
+        private void ShowMessageBoxModel(bool isyes = false, string msg = "", bool isAsync = false)
         {
-            CustomMessageBox msgBox=new CustomMessageBox(isyes,msg);
+            CustomMessageBox msgBox = new CustomMessageBox(isyes, msg);
             RectangleGeometry rect = new RectangleGeometry();
             rect.Rect = new Rect(0, 50, 300, 108);
             rect.RadiusX = 10;
@@ -230,6 +293,11 @@ namespace SocinatorInstaller
         }
 
         private void click_CancelBtn(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
         {
 
         }
